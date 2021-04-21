8cfd66e7bb77b63fbd04af69c92c8229b14ffd16
Hharshitsingh <realharshitsingh@gmail.com>
2021-04-22 00:27:28 +0530

    update

--- opencv/face_detaction/face_recognition.py
+++ opencv/face_detaction/face_recognition.py
@@ -3,17 +3,17 @@ import cv2 as cv
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-people = ['Harshit Singh' ,'Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 # features = np.load('./features.npy', allow_pickel =True)
 # lables = np.load('./labels.npy')
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('./face_trained.yml')
 
-img = cv.imread('../Resources/Faces/val/Harshit Singh/2.png')
+img = cv.imread('../Resources/Faces/val/elton_john/2.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Person', gray)
+# cv.imshow('Person', gray)
 
 #Detect the face in thr image
 faces_react = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
