74af8b88050562459e688b98fcbdac6496a6923f
Hharshitsingh <realharshitsingh@gmail.com>
2021-07-16 00:30:52 +0530

    update

---
+++ Program/45.test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+edge_diver = r""
+driver = webdriver.Edge(executable_path=edge_diver)
+
+driver.get("https://github.com/Hharshitsingh")
+for i in range(1,10):
+    time.sleep(0.5)
+    driver.refresh()
+driver.quit()
